817d7b98864fb43db5d70984d00e5a90aa0fa630
Amanda <108832388+shvistak@users.noreply.github.com>
2024-05-22 15:05:03 -0300

    2205

--- main.js
+++ main.js
@@ -7,13 +7,27 @@ botoes[0].onclick = diminuiTamanho;
 botoes[1].onclick = aumentaTamanho;
 function diminuiTamanho(){
     if(tamanhoSenha > 1){
-    tamanhoSenha = tamanhoSenha - 1;
+    //tamanhoSenha = tamanhoSenha - 1;
     }
     numeroSenha.textContent = tamanhoSenha;
+    geraSenha();
 }
 function aumentaTamanho(){
     if(tamanhoSenha < 20){
-    tamanhoSenha = tamanhoSenha + 1;
+    //tamanhoSenha = tamanhoSenha + 1;
     }
     numeroSenha.textContent = tamanhoSenha;
+    geraSenha();
 }
+// codigo omitido
+const campoSenha = document.querySelector('#campo-senha');
+const letrasMaiusculas = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+function geraSenha(){
+    let senha = '';
+    for (let i = 0; i < tamanhoSenha; i++){
+    let numeroAleatorio = Math.random()*letrasMaiusculas.length;
+    numeroAleatorio = Math.floor(numeroAleatorio);
+    senha = senha + letrasMaiusculas[numeroAleatorio];
+    }
+}
+campoSenha.value = senha;
