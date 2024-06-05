d10b8587a30d764e0a3c8b4708416076ad9e12f2
Amanda <108832388+shvistak@users.noreply.github.com>
2024-06-05 15:08:06 -0300

    0506

--- main.js
+++ main.js
@@ -1,57 +1,57 @@
-document.querySelector('.parametro-senha-botao');
 const numeroSenha = document.querySelector('.parametro-senha-texto');
 let tamanhoSenha = 12;
-senha.textContent = tamanhoSenha;
-const botoes = document.querySelector('.parametro-senha-botao');
+numeroSenha.textContent = tamanhoSenha;
+const checkbox = document.querySelectorAll('.checkbox');
+const botoes = document.querySelectorAll('.parametro-senha-botao'); 
 const forcaSenha = document.querySelector('.forca');
 botoes[0].onclick = diminuiTamanho;
 botoes[1].onclick = aumentaTamanho;
 function diminuiTamanho(){
-    if(tamanhoSenha > 1){
-    //tamanhoSenha = tamanhoSenha - 1;
+    if (tamanhoSenha > 1){
+       // tamanhoSenha = tamanhoSenha-1;
+        tamanhoSenha--;
     }
     numeroSenha.textContent = tamanhoSenha;
     geraSenha();
 }
 function aumentaTamanho(){
-    if(tamanhoSenha < 20){
-    //tamanhoSenha = tamanhoSenha + 1;
+    if (tamanhoSenha < 20){
+       // tamanhoSenha = tamanhoSenha+1;
+       tamanhoSenha++;
     }
     numeroSenha.textContent = tamanhoSenha;
     geraSenha();
 }
+
 // codigo omitido
 const campoSenha = document.querySelector('#campo-senha');
 const letrasMaiusculas = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-
+const letrasMinusculas = 'abcdefghijklmnopqrstuvxywz';
+const numeros = '0123456789';
+const simbolos = '!@%*?';
+const valorEntropia = document.querySelector('entropia');
 function geraSenha(){
-    let senha = '';
-    for (let i = 0; i < tamanhoSenha; i++){
-    let numeroAleatorio = Math.random()*letrasMaiusculas.length;
-    numeroAleatorio = Math.floor(numeroAleatorio);
-    senha = senha + letrasMaiusculas[numeroAleatorio];
+    let alfabeto = '';
+    if (checkbox[0].checked){
+    alfabeto = alfabeto + letrasMaiusculas;
     }
-}
-campoSenha.value = senha;
-const checkbox = document.querySelectorAll(‘.checkbox’);
-console.log(checkbox);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    classificaSenha(alfabeto.lenght);
+    if (checkbox[1].checked){
+    alfabeto = alfabeto + letrasMinusculas;
+    }
+    if (checkbox[2].checked){
+    alfabeto = alfabeto + numeros;
+    }
+    if (checkbox[3].checked){
+    alfabeto = alfabeto + simbolos;
+    }
+    let senha = '';
+    for (let i = 0; i < tamanhoSenha;i++){
+    let numeroAleatorio = Math.random()*alfabeto.length;
+    numeroAleatorio = Math.floor(numeroAleatorio);
+    senha = senha + alfabeto[numeroAleatorio];
+    }
+    campoSenha.value = senha;
+    classificaSenha(alfabeto.length);
     }
 function classificaSenha(tamanhoAlfabeto){
     let entropia = tamanhoSenha * Math.log2(alfabeto.length);
